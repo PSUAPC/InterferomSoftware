589350fb1498e848a3269406a1fc270f4bde974a
vasimr <jfixelle@gmail.com>
2016-06-25 23:54:14 -0500

    [Migrated] Ncurses code to NWindow

--- RDTestPrograms/NCursesTerminalTest/src/NWindow.cpp
+++ RDTestPrograms/NCursesTerminalTest/src/NWindow.cpp
@@ -1,1 +1,122 @@
 #include "NWindow.h"
+#include <signal.h>
+
+// singleton global variable for the window
+NWindow* g_Window = NULL;
+
+
+NWindow::NWindow()
+	: IWidget(NULL), m_Win(NULL)
+{
+	// singleton assignment
+	if( g_Window == NULL )
+	{
+		g_Window = this;
+	}
+	else
+	{
+		throw "Atempting to create a second Window, which is of type singleton";
+	}
+}
+
+NWindow::~NWindow()
+{
+	// set the singleton global to null
+	g_Window = NULL;
+}
+
+NWindow* NWindow::Get()
+{ 
+	// static function get the singleton instance
+	return g_Window; 
+}
+
+void NWindow::Init()
+{
+	if( !m_Inhibit )
+		return; // something is wrong
+
+	m_Win = initscr();			// create the screen
+	noecho();				// don't echo to the screen
+	keypad(m_Win, TRUE);	 		// enable keypad
+	cbreak();				// disable break commands
+	getmaxyx(m_Win, m_Rows, m_Cols );	// get the screen size
+	clear();				// clear the screen
+	refresh();	
+	mvprintw(m_Rows-1, 0, ">");		// print the command prompt
+	refresh();
+	signal(SIGWINCH, &NWindow::OnResize); // register the resize function
+	m_Inhibit = false;			// allow the terminal to function
+}
+
+void NWindow::Shutdown()
+{
+	m_Inhibit = true;
+	endwin();
+}
+
+void NWindow::Redraw()
+{
+	if( m_Inhibit ) return;
+
+	pthread_mutex_lock(&m_Mutex);
+	
+	// clear the window
+	clear();
+
+	// refresh
+	refresh();
+
+	pthread_mutex_unlock(&m_Mutex);
+
+}
+
+void NWindow::OnResize(int param)
+{
+
+	if( g_Window != NULL )
+	{
+		if( g_Window->m_Inhibit ) return;
+
+		// get the new size
+		getmaxyx(stdscr, g_Window->m_Rows, g_Window->m_Cols);
+		
+		// force a redraw
+		g_Window->Redraw();
+	}
+}
+
+std::string NWindow::WaitForInput()
+{
+
+	std::string temp = "";
+
+	if( m_Inhibit ) return temp;
+	
+	// set timeout
+	//timeout(10);
+
+	int c = wgetch(m_Win);
+	
+	pthread_mutex_lock(&m_Mutex);
+
+	switch( c )
+	{
+	// error character
+	case ERR:
+		break;
+	case KEY_STAB:
+		break;
+	default:
+		break;
+	}
+
+	pthread_mutex_unlock(&m_Mutex);
+
+	Redraw();
+	return temp;
+}
+
+
+
+
--- RDTestPrograms/NCursesTerminalTest/src/NWindow.h
+++ RDTestPrograms/NCursesTerminalTest/src/NWindow.h
@@ -1,7 +1,36 @@
-#ifndef __NWIN_H__
-#define __NWIN_H__
+#ifndef __NWINDOW_H__
+#define __NWINDOW_H__
+
+#include "Includes.h"
+#include <ncurses.h>
+#include <string>
+#include <list>
+#include "WidgetInterface.h"
+
+class NWindow : public IWidget
+{
+	typedef std::list<std::string> History;
+public:
+	NWindow();
+	~NWindow();
+
+	static NWindow* Get();
+	void Init();
+	void Shutdown();
+	void Redraw();
+	void Inhibit(){ m_Inhibit = true; }
+	std::string WaitForInput();
+	void SetMutex(pthread_mutex_t mutex){ m_Mutex = mutex; }
+private:
+	pthread_mutex_t m_Mutex;
+	int m_Rows;
+	int m_Cols;
+	bool m_Inhibit;
+	WINDOW* m_Win;
+	static void OnResize(int param);
+};
 
 
 
 
-#endif //__NWIN_H__
+#endif //__NWINDOW_H__
